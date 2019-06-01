da8547e39077f51b09941352567ab2c421348d89
hellojohnwoo <hellojohnwoo@gmail.com>
2019-06-02 00:00:19 +0900

    0602 java-bronze

---
+++ src/Mondai002.java
@@ -0,0 +1,10 @@
+public class Mondai002 {
+    public static void main(String[] args) {
+        int b[] = {1,  2,  3,  4};
+        for (int a : b) {           // 拡張for文   // for (変数宣言：参照変数名）
+            System.out.print (a);
+        }
+    }
+}
+
+// 1234
